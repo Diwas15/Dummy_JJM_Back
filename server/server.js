47785351bd0466341780f1f239c506532b376205
Diwas Sati <satidiwas@gmail.com>
2024-11-09 20:46:25 +0530

    old

--- server/server.js
+++ server/server.js
@@ -244,10 +244,10 @@ app.post('/grievance',(req,res)=>{
 })
 
 
-app.use(express.static(path.join(__dirname,"../../frontend/build")));
-app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname,"../../frontend/build/index.html"))
-})
+// app.use(express.static(path.join(__dirname,"../../frontend/build")));
+// app.get("*",(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,"../../frontend/build/index.html"))
+// })
 
 app.listen(port,'0.0.0.0',()=>{
     console.log(`server running on port ${port}`);
